2bb7fa455dbb7f3f8513fa634fb71a796cd7d2ea
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-10-31 17:41:11 -0400

    Use meta package

--- lib/category.dart
+++ lib/category.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 final _rowHeight = 100.0;
 final _borderRadius = BorderRadius.circular(_rowHeight / 2);
