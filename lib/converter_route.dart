d2f6092f266d99e5df819a415e51157084ac0ec4
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-11-03 14:40:33 -0400

    Make widgets stateful

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -2,16 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'package:unit_converter/unit.dart';
 
-class ConverterRoute extends StatelessWidget {
+const _padding = EdgeInsets.all(16.0);
+
+class ConverterRoute extends StatefulWidget {
+  final Color color;
+
   final List<Unit> units;
+
   const ConverterRoute({
+    @required this.color,
     @required this.units,
-  }) : assert(units != null);
+  })  : assert(color != null),
+        assert(units != null);
+
+  @override
+  _ConverterRouteState createState() => _ConverterRouteState();
+}
+
+class _ConverterRouteState extends State<ConverterRoute> {
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
+  }
 
   @override
   Widget build(BuildContext context) {
-    final unitWidgets = units.map((Unit unit) {
+    final unitWidgets = widget.units.map((Unit unit) {
       return Container(
+        color: widget.color,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
